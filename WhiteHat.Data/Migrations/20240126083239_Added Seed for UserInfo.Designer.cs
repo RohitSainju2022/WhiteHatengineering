13558dff1fcd7a66e0a4d554a1c55e5ff8abe4d5
rohit <rohit@codebee.dk>
2024-01-26 14:19:39 +0545

    WhiteHat-3 Added Seeders for userinfo

--- WhiteHat.Data/Migrations/20240126083239_Added Seed for UserInfo.Designer.cs
+++ WhiteHat.Data/Migrations/20240126083239_Added Seed for UserInfo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteHat.Data.WhiteHatDbContext;
 
@@ -11,9 +12,11 @@ using WhiteHat.Data.WhiteHatDbContext;
 namespace WhiteHat.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240126083239_Added Seed for UserInfo")]
+    partial class AddedSeedforUserInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WhiteHat.Data/Migrations/20240126083239_Added Seed for UserInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteHat.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedforUserInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserInfoes",
+                columns: new[] { "UserId", "DOB", "FirstName", "GenderId", "Image", "IsDeleted", "LastName", "MiddleName", "Mobile" },
+                values: new object[] { new Guid("9668dc45-23af-4cae-a85b-3711bb1b4247"), null, "Test", 1, null, false, "User", null, "+9808311151" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserInfoes",
+                keyColumn: "UserId",
+                keyValue: new Guid("9668dc45-23af-4cae-a85b-3711bb1b4247"));
+        }
+    }
+}
